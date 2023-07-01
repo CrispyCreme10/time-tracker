6c74b243c11f5d331a99f3880a4202951eddf86a
Jordan R <jordanrenner69@gmail.com>
2023-07-01 00:36:04 -0500

    yoink doink

---
+++ index.js
@@ -0,0 +1,50 @@
+const {app, BrowserWindow, ipcMain} = require('electron')
+const url = require("url");
+const path = require("path");
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  win.loadURL('http://localhost:4200');
+
+  win.on('closed', function () {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+// ------->
+
+ipcMain.on('close', () => {
+  win.hide();
+});
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
+
+type DayOfWeek = 'Sun' | 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri' | 'Sat';
+
+interface DaySession {
+  day: DayOfWeek;
+  sessions: Session[]
+}
+
+interface Session {
+  start: Date,
+  end: Date,
+  notes: string
+}
+
+const DEFAULT_TIME = '00:00:00';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +23,94 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  @ViewChild('notes') notes!: ElementRef;
+
+  timerStopped = true;
+  interval!: ReturnType<typeof setTimeout>;
+  startTime = 0; // in ms since start date value
+  timeElapsed = 0; // in ms
+  timerFormatted: string = DEFAULT_TIME;
+
+  todaysSessions: Session[] = [];
+  weeksSessions: DaySession[] = [];
+
+  todayExpanded = false;
+  weeksExpanded = false;
+  weekDaysExpandedMap = {
+    ['Sun']: false,
+    ['Mon']: false,
+    ['Tue']: false,
+    ['Wed']: false,
+    ['Thu']: false,
+    ['Fri']: false,
+    ['Sat']: false,
+  };
+
+  faCaretRight = faCaretRight;
+
   // middle area for start/stop + description + submit
   // left area for details about today's laps
   // right area for details about this week's times
   //  could display a single row for all days this week
   //  could dropdown each day to see list of each lap for that day
+
+  startTimer() {
+    if (this.timeElapsed === 0) {
+      this.timerStopped = false;
+      this.startTime = Date.now();
+    }
+
+    if (!this.timerStopped && this.timeElapsed > 0) {
+      this.pauseTimer();
+    } else {
+      this.interval = setInterval(() => {
+        this.timeElapsed = Date.now() - this.startTime;
+        this.formatTime();
+      }, 1000);
+    }
+  }
+
+  private pauseTimer() {
+    this.timerStopped = true;
+    clearInterval(this.interval);
+  }
+
+  resetTimer() {
+    this.timeElapsed = 0;
+    this.timerFormatted = DEFAULT_TIME;
+    this.pauseTimer();
+  }
+
+  formatTime() {
+    const seconds = this.timeElapsed / 1000;
+    const hours = seconds / 3600;
+    const mins = seconds / 60;
+    const secs = seconds % 60;
+    this.timerFormatted = `${(hours < 10 ? '0' : '') + Math.floor(hours)}:${(mins < 10 ? '0' : '') + Math.floor(mins)}:${(secs < 10 ? '0' : '') + Math.floor(secs)}`;
+  }
+
+  submit() {
+    // save start time, end time & notes to file
+    const startDateTime = new Date(this.startTime);
+    const endDateTime = new Date(this.startTime + this.timeElapsed);
+
+    console.log('start time: ', startDateTime);
+    console.log('time elapsed: ', this.timerFormatted);
+    console.log('end time: ', endDateTime);
+  }
+
+  expandToday() {
+    this.todayExpanded = !this.todayExpanded;
+  }
+
+  expandWeek() {
+    this.weeksExpanded = !this.weeksExpanded;
+  }
+
+  expandDayOfWeek(day: DayOfWeek) {
+    this.weekDaysExpandedMap[day] = !this.weekDaysExpandedMap[day];
+  }
+
   close() {
     window.ipcRenderer.send('close');
   }
